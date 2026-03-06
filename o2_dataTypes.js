03a66ee0af165fefaf309746fdba0c30bb183576
Muskan Shukla <muskanshukla187@gmail.com>
2026-03-06 19:08:09 +0530

    add dataTypes and test files

---
+++ o2_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"  //treat all js code as newer version
+// alert(3+3)   // we are using node js ,not browser
+
+// data types
+/*
+primitiv data types
+
+number 
+bigit
+string =>""
+boolean  =>true /false
+null  =>standalon value
+undefined =>vlue not assign
+symbol ==> assign uniquness
+
+non -primitive
+
+Object
+*/
+
+console.log(typeof undefined);
+console.log(typeof null);
