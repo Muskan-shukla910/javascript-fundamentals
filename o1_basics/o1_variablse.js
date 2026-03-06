4b0dfc6028fefa651b562cd0e0e044d1b9ca05ac
Muskan Shukla <muskanshukla187@gmail.com>
2026-03-06 19:00:04 +0530

    variables

---
+++ o1_basics/o1_variablse.js
@@ -0,0 +1,20 @@
+const accoundId=123;
+let accountEmail="muskan123";
+var accountPass="12345";
+accoundCity="Jaipur";
+let accountState;
+
+// Cannot redeclare 
+// Cannot reasign
+// accoundId=124;  Not allowed
+// console.log(accoundId)
+accountCity="Lucknow";
+accountEmail="User@123";
+accountPass="321";
+console.table([accountCity,accountEmail,accountPass,accountState]);
+
+/*
+Prefer not to use var
+because of issues in block scope and functional scope 
+*/
+
