59e9e6332ee88f849481ab30a5755c2905c459cb
Muskan Shukla <muskanshukla187@gmail.com>
2026-03-07 13:36:29 +0530

    conversionOparations

---
+++ o1_basics/o3_conversionOpration.js
@@ -0,0 +1,24 @@
+// let num=true;
+// console.log(typeof num);
+// console.log(typeof (num));
+
+// let valueInNUmber=Number(num);
+// console.log(typeof valueInNUmber);
+// console.log(valueInNUmber);
+
+// // "33" => 33
+// "33abc" => NaN(Not a number)
+// true => 1; false => 0
+// undefined => NaN
+ let isLogeedIn='Muskan';
+ let booleanIsLoggedIn=Boolean(isLogeedIn);
+//  console.log(booleanIsLoggedIn);
+
+//  1 => true
+// 0 => false
+//  '' => false
+// "Muskan" => true
+
+let someNunber= 33;
+let stringNumber=String(someNunber);
+console.log(typeof stringNumber);
