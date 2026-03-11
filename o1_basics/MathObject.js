65d9e4b832bffe95ff281cb67077e6cd9c158128
Muskan Shukla <muskanshukla187@gmail.com>
2026-03-11 17:25:03 +0530

    MathObject

---
+++ o1_basics/MathObject.js
@@ -0,0 +1,21 @@
+let num=10;
+// console.log(Math.floor(Math.random()*10)+1);
+// console.log(Math.floor(6,3));
+
+
+
+// console.log(Math.floor(Math.random()*6+1));
+console.log(Math.floor(Math.random()*(25-15+1))+15);
+
+// OTP Generate
+console.log(Math.floor(Math.random()*(9999-1000+1))+1000);
+
+// Random function
+function random(){
+
+}
+let n=1204;
+
+const now=new Date();
+// console.log(now.getDay());
+console.log(now.getDate());                                                                    
