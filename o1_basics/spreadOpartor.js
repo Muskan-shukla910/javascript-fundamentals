4dfd16650a3f7f63e806eaf8551842498fa815a2
Muskan Shukla <muskanshukla187@gmail.com>
2026-03-12 11:41:28 +0530

    Spread Oprator

--- o1_basics/spreadOpartor.js
+++ o1_basics/spreadOpartor.js
@@ -1,6 +1,14 @@
 let arr=[10,30,50,70];
 let arr1=["Rohit",11,true];
+let arr3=[90,4,false];
 // arr.push(arr1);
 // console.log(arr);
-let arr3=arr.concat(arr1);
-console.log(arr3);
+// let arr3=arr.concat(arr1);
+// console.log(arr3);
+// const arr4=[arr,arr1,arr3];
+
+const arr4=[...arr,...arr1,...arr3];
+console.log(arr4);
+
+
+
