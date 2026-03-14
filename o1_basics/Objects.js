308c6782cbe8d36cfabe895bce646e7111c688d0
Muskan Shukla <muskanshukla187@gmail.com>
2026-03-14 13:04:03 +0530

    Objects

--- o1_basics/Objects.js
+++ o1_basics/Objects.js
@@ -1,2 +1,44 @@
-let a="muskan Shukla";
-console.log(a);
+// Object
+// Key value
+
+// const { use } = require("react");
+
+const user={
+    name:"Rohit",
+    age:20,
+    emailId:"user@gmail.com",
+    amount:3400,
+    "home adress":"Lucknow"
+}
+// console.log(user);
+console.log(user.age);
+
+// CRUD (create,delete,update,read)
+
+// Update
+user.addhar=12345;
+user.amount=9000;
+
+console.log(user);
+
+// delete
+delete user.emailId;
+console.log(user);
+
+// Acess the element
+console.log(user["name"]);
+console.log(user.age);
+console.log(user["home adress"]);
+
+
+// Important 
+
+console.log(Object.keys(user));
+console.log(Object.values(user));
+console.log(Object.entries(user));
+
+
+// For in loop
+for(let keys in user){
+    console.log(keys,user[keys]);
+}
