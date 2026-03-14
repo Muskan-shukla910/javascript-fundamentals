d38bbc11861034da234fc217986252bb6b433fb5
Muskan Shukla <muskanshukla187@gmail.com>
2026-03-14 16:11:12 +0530

    Detstrucring

---
+++ o2_basics/o1_objectsDestrucring.js
@@ -0,0 +1,41 @@
+// const { use } = require("react");
+
+// const { use } = require("react");
+
+// Object destructing
+const user={
+    name:"Muskan",
+    age:22,
+    course:"BCA",
+    id:123
+}
+
+// destrucring
+
+
+// const {name,age,course}=user;
+// console.log(name,age,course);
+
+// without destrucring
+// const name=user.name;
+// const age=user.age;
+// console.log(name,age);
+
+// Array destrucring
+const arr=[21,3,43,56,78,90];
+const [first,secound,third]=arr;
+console.log(first,secound,third);
+
+// update using destrucring
+const {name:userName,age:userAge,course}=user;
+console.log(userName,userAge,course);
+
+// For of loop 
+
+// for(let keys of Object.values(user)){
+    // console.log(keys);
+// }
+
+for(let keyVaues of Object.entries(user)){
+    console.log(keyVaues);
+}
