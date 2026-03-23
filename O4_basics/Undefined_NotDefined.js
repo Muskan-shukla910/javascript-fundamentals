0b5ce25cb276e71ac3c1203aa3406b8c31a14e84
Muskan Shukla <muskanshukla187@gmail.com>
2026-03-23 12:44:20 +0530

    Undefined_NotDefined

---
+++ O4_basics/Undefined_NotDefined.js
@@ -0,0 +1,28 @@
+console.log(a);
+
+var a;
+// console.log(b);
+
+// var a=10;
+// console.log(a);
+
+if(a===undefined){
+    console.log("a is undefined");
+}
+else{
+    console.log("a is not defined");
+}
+
+
+// js is loosly type languange
+
+
+var a=10;
+console.log(a);
+
+var a="Muskan";
+console.log(a);
+
+var a=undefined;
+console.log(a);
+
