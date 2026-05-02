b6b2813583d26ad380d5e7b7c6dee8f562779575
Muskan Shukla <muskanshukla187@gmail.com>
2026-05-02 21:41:08 +0530

    Control Flow

---
+++ Control flow/script.js
@@ -0,0 +1,16 @@
+// const score = 200;
+// if (score > 100) {
+//     const power = "fly";
+//     console.log(`User Power: ${power}`);
+// }
+// console.log(`User Power: ${power}`);
+
+const balance =1000;
+if(balance>10000){
+    console.log("hey");    
+}
+else{
+    // console.log()
+    console.log("Balance is greater than 1000");
+
+}
