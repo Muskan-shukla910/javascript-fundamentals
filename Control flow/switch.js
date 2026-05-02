73e738bd2cfab5ef1a0782e02b9f81bef103ddde
Muskan Shukla <muskanshukla187@gmail.com>
2026-05-02 22:19:17 +0530

    switch

---
+++ Control flow/switch.js
@@ -0,0 +1,55 @@
+// // switch (key) {
+// //     case value:
+        
+// //         break;
+
+// //     default:
+// //         break;
+// // }
+
+// const month=3;
+// switch (month) {
+//     case 1:
+//         console.log("Yes this is one");
+//         break;
+//     case 2:
+//         console.log("Yes this is two");
+//         break;
+//     case 3:
+//         console.log("Yes this is three");
+//         break;
+//     case 4:
+//         console.log("Yes this is four");
+//         break;
+//     case 5:
+//         console.log("Yes this is five");
+//         break;
+//     case 6:
+//         console.log("Yes this is six");
+//         break;
+//     case 7:
+//         console.log("Yes this is seven");
+//         break;
+
+//     default:
+//         break;
+// }
+const month="april"
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("february"); 
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;           
+    default:
+        console.log("no one monthh match here ");
+        
+        break;       
+}
