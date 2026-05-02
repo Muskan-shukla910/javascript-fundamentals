e0f1054e4e5aa3ab58e27b9c2fc9bb79d00aef18
Muskan Shukla <muskanshukla187@gmail.com>
2026-05-02 22:10:23 +0530

    COntrol flow

--- Control flow/script.js
+++ Control flow/script.js
@@ -5,12 +5,26 @@
 // }
 // console.log(`User Power: ${power}`);
 
-const balance =1000;
-if(balance>10000){
-    console.log("hey");    
-}
-else{
-    // console.log()
-    console.log("Balance is greater than 1000");
+// const balance =1000;
+// if(balance>10000){
+//     // console.log("hey");    
+// }
+// else{
+//     // console.log()
+//     // console.log("Balance is greater than 1000");
 
-}
+// }
+
+// const userLoggedIn=true
+// const debitCart=true
+// const loggedInFromGoogle=false
+// const loggedInFromGmail=true
+// if(userLoggedIn && debitCart && 2==3){
+//     console.log("Allow to buy course");
+// }
+// if(loggedInFromGmail || loggedInFromGoogle){
+//     console.log("User Logged in");
+    
+// }
+
+
