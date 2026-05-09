5e74b48a0f7b03c4a351d98a98bad259316e7853
Muskan Shukla <muskanshukla187@gmail.com>
2026-05-09 23:40:59 +0530

    arrayMethds

--- Arrays/filter.js
+++ Arrays/filter.js
@@ -6,9 +6,25 @@
 
 // reduce  jb hume last me output ek hi number bne 
 
-let arr=[0,90,70,0,7];
+// let arr=[0,90,70,0,7];
+// let newArr=arr.reduce((accumaloter,val)=>{
+//     return accumaloter+val;
+// },0);
+// console.log(newArr);
 
-let newArr=arr.reduce((accumaloter,val)=>{
-    return accumaloter+val;
-},0);
-console.log(newArr);
+
+// let arr=[1,2,3,4,5];
+// destrcturing
+// let [a,b, ,c]=arr;
+// console.log(arr);
+
+// Spread Oprator
+let arr=[1,2,3,4,5,6,7,8,9,0];
+// esme array ka refernce gya hai na ki copy array kii
+// let arr2=arr;
+let arr2=[...arr];
+console.log(arr);
+
+
+// Bakki jagaho pr spread opartor ko rest opatorbbolte hai 
+
