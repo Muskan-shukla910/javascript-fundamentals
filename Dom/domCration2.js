d80d68032252a3a6438fcd13824f5ea70c163b01
Muskan Shukla <muskanshukla187@gmail.com>
2026-05-23 21:56:19 +0530

    dom

---
+++ Dom/domCration2.js
@@ -0,0 +1,5 @@
+let h2=document.createElement("h1");
+h2.textContent="Mai muskan hoo tum kaun ?";
+document.querySelector('div').append(h2);
+
+h2.style.color="red";
--- Dom/index.js
+++ Dom/index.js
@@ -1,4 +1,5 @@
 let heading1=document.querySelector("h1");
+// agr kisi elements ki deyails hume console.log nhii mil rha hai toh console.log(dir) krenge
 // console.dir(heading1);
 // Text content acess:  innerText,InnerContent, innerHtml
 // heading1.innerHTML="Hello Muskan kaisi ho";
@@ -12,4 +13,13 @@ heading1.hidden=false;
 let a =document.querySelector("a");
 // Attribute manipulation
 // We can also manipulate using get and set attribute
-a.href="http://google.com"
+// set atribute
+// a.setAttribute("href","https://www.google.com");
+// console.log(a.getAttribute("href"));
+a.removeAttribute("href")
+let a1=document.getElementsByClassName("abcd");
+console.log(a1);
+let img1=document.querySelector("img");
+// img1.setAttribute("src","image adress");
+
+
