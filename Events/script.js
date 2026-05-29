e7d301321826694d9524a26ccd27a378e1be85c0
Muskan Shukla <muskanshukla187@gmail.com>
2026-05-29 11:34:51 +0530

    ChangeEvents

--- Events/script.js
+++ Events/script.js
@@ -8,9 +8,36 @@
 // });
 
 
-let sel=document.querySelector("select");
-let h1=document.querySelector("h2");
-sel.addEventListener("change" ,function(dets){
-    console.log();
-    h1.textContent=`${dets.target.value} device selected`;
+// let sel=document.querySelector("select");
+// let h1=document.querySelector("h2");
+// sel.addEventListener("change" ,function(dets){
+//     console.log();
+//     h1.textContent=`${dets.target.value} device selected`;
+// });
+
+let h1 = document.querySelector("h1");
+
+window.addEventListener("keydown", function (dets) {
+    console.log(dets.key);
+    h1.textContent = `${dets.key}`
 });
+let inp = document.querySelector("input");
+let btn = document.querySelector("#btn");
+btn.addEventListener("click", function () {
+    inp.click();
+
+})
+inp.addEventListener("change", function (dets) {
+    // console.log(dets.target.value);
+    console.log(dets.target.files[0].name);
+    // console.log(dets);
+    // if (dets.target.files) {
+    //     btn.textContent = dets.target.files[0].name;
+    // }
+
+    // more simple and clear code 
+    const file=dets.target.files[0];
+    if(file){
+        btn.textContent=file.name;
+    }
+})
