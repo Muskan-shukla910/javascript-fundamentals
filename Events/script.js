21cb0781c2a806f7212ef8f5dd88a1f823e0cc89
Muskan Shukla <muskanshukla187@gmail.com>
2026-05-29 17:44:10 +0530

    events

--- Events/script.js
+++ Events/script.js
@@ -45,49 +45,67 @@
 
 
 // Input validation
-
-let inputs=document.querySelectorAll("input");
-let form=document.querySelector("form");
-let main=document.querySelector("#main");
-form.addEventListener("submit",function(dets){
-    // console.log(dets);
+// form validation
+// let inputs=document.querySelectorAll("input");
+// let form=document.querySelector("form");
+// let main=document.querySelector("#main");
+// form.addEventListener("submit",function(dets){
+//     // console.log(dets);
     
-    dets.preventDefault();
-  let card= document.createElement("div");
-  card.classList.add("card");
-  let profile=document.createElement("div");
-  profile.classList.add("profile");
-//   added profile in card 
+//     dets.preventDefault();
+//   let card= document.createElement("div");
+//   card.classList.add("card");
+//   let profile=document.createElement("div");
+//   profile.classList.add("profile");
+// //   added profile in card 
 
-let img=document.createElement("img");
-img.setAttribute("src","SSOC.jpeg");
+// let img=document.createElement("img");
+// img.setAttribute("src","SSOC.jpeg");
   
 
-let h3=document.createElement("h3");
-h3.textContent=inputs[1].value;
-let h5=document.createElement("h5");
-h5.textContent=inputs[2].value;
-let p=document.createElement("p");
-p.textContent=inputs[3].value;
+// let h3=document.createElement("h3");
+// h3.textContent=inputs[1].value;
+// let h5=document.createElement("h5");
+// h5.textContent=inputs[2].value;
+// let p=document.createElement("p");
+// p.textContent=inputs[3].value;
 
 
-profile.appendChild(img);
-card.appendChild(profile);
+// profile.appendChild(img);
+// card.appendChild(profile);
 
-card.appendChild(h3)
-card.appendChild(h5)
-card.appendChild(p);
+// card.appendChild(h3)
+// card.appendChild(h5)
+// card.appendChild(p);
 
-main.appendChild(card);
+// main.appendChild(card);
 
-inputs.forEach((inp)=>{
-    if(inp.type !== "submit"){
-        inp.value=" ";
-    }
-    // inp.value=" ";
-})
+// inputs.forEach((inp)=>{
+//     if(inp.type !== "submit"){
+//         inp.value=" ";
+//     }
+//     // inp.value=" ";
+// })
+// });
+
+
+
+// mouseOver and Mousemove
+
+let a=document.querySelector("#a");
+// a.addEventListener("mouseover",function(){
+// abc.style.backgroundColor="yellow"
+// });
+
+window.addEventListener("mousemove",function(dets){
+    a.style.top=dets.clienty+"px";
+    a.style.left=dets.clientX+"px";
 });
 
+window.addEventListener("mousemove",function(dets){
+    console.log(dets);
+    
+})
 
 
 
