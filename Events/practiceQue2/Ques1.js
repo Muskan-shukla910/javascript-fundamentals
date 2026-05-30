050d798b7a2c82c3358f692f9c5320e5bd0ecf76
Muskan Shukla <muskanshukla187@gmail.com>
2026-05-30 22:10:45 +0530

    Qustions

---
+++ Events/practiceQue2/Ques1.js
@@ -0,0 +1,17 @@
+//  Try out the following events in Event Listener on your own :- mouseout- keypress- Scroll- load [Use MDN for help]
+
+
+let btn=document.querySelector("button");
+btn.textContent="click me"
+btn.addEventListener("click",function(){
+    btn.style.color="green";
+});
+
+let h2=document.querySelector("h2");
+let name=document.querySelector("input");
+
+let form=document.querySelector("form");
+form.addEventListener("onchange",function(){
+    name.
+})
+
---
+++ Events/practiceQuetions/Que1.js
@@ -0,0 +1,14 @@
+let inp=document.createElement("input");
+let button=document.querySelector("button");
+
+
+button.addEventListener("click",function(){
+    button.textContent="click me ";
+});
+
+
+inp.setAttribute("placeholder","username");
+
+
+
+
