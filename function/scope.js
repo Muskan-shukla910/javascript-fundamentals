76f41302ddc7f56235bc10ce46a91d4b5173ea2b
Muskan Shukla <muskanshukla187@gmail.com>
2026-05-30 18:07:23 +0530

    Scope

---
+++ function/scope.js
@@ -0,0 +1,16 @@
+// scope > functional scope ,global scope, block scope
+
+// functional scope:- can be used inside function
+// global scope:- can be used entire code anywehre
+// block scope :- {} curly braces hi use ho sakte hai 
+
+function abcd(){
+    var a=12;
+}
+
+// execution content 
+// it is a hypothetically container
+// this done using two phase memory and execution phase first phase me memory allocation hota hai and secounf me code actually me run hota hai 
+
+
+// .js lexical scoping and dynmically scoping 
